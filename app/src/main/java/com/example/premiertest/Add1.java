b9ed62aae409af4f97702b727859ffb7d115fd4a
Philigaudron <philippine.gaudron@epfedu.fr>
2020-03-18 21:26:36 +0100

    Ajout du bouton suivant Effaçage de l'édit text en cours Ajour du compteur Problème quand on clique sur ok alors qu'il n'y a rien dans remplir (edit text) ca beug

--- app/src/main/java/com/example/premiertest/Add1.java
+++ app/src/main/java/com/example/premiertest/Add1.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 
@@ -20,6 +21,8 @@ public class Add1 extends AppCompatActivity {
     boolean go=true;
     Integer result;
     Integer result1;
+    Integer cpt=1;
+    Integer triche=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +53,9 @@ public class Add1 extends AppCompatActivity {
         test.setText(""+a+" + "+b+" =  ?");
         result = a+b; //Calcul du résultat attendu
 
+        TextView cpta = (TextView) findViewById(R.id.cpta);
+        cpta.setText(""+cpt+" / 10");
+
 
         //Récupére la valeur d'édit text et la vérifie
         Button ok = findViewById(R.id.ok);
@@ -60,6 +66,14 @@ public class Add1 extends AppCompatActivity {
                 EditText remplir = (EditText)findViewById(R.id.remplir);
                 int nb = (int) Integer.parseInt(remplir.getText().toString());
 
+
+                /* Pour éviter que lorsqu'on appuie sur OK sans edit text ca crash
+                                if(remplir.getText()==null){
+                    TextView resultat = (TextView) findViewById(R.id.resultat);
+                    resultat.setText("ERREUR");
+                }else
+                 */
+
                 Verification(result,nb);
                 CalculScore(sucess);
 
@@ -67,30 +81,40 @@ public class Add1 extends AppCompatActivity {
                 affichagescore.setText("Score : "+score);
                 remplir.getText().clear();
 
-                //Fonction pour ne pas tricher
-                /*
-                if(){
-                    ok.setClickable(false);
-                }
-                 */
-
-
 
             }
         });
+
+
+        //Fonction pour ne pas tricher
+        /*
+                if(triche ==1){
+            ok.setEnabled(false);
+        }else
+            if(triche==0){
+                ok.setEnabled(true);
+            }
+         */
+
+
 
         Button suivant = findViewById(R.id.suivant);
         suivant.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                triche=0;
                 int c = 1 + (int) (Math.random() * (100));
                 int d = 1 + (int) (Math.random() * (100));
                 TextView test = (TextView) findViewById(R.id.test);
                 test.setText(""+c+" + "+d+" =  ?");
 
                 result = c+d; //Calcul du résultat attendu
+                cpt=cpt+1;
 
-
+                //Affichage du nbr de questions jouées
+                TextView cpta = (TextView) findViewById(R.id.cpta);
+                cpta.setText(""+cpt+" / 10");
             }
         });
 
@@ -116,18 +140,20 @@ public class Add1 extends AppCompatActivity {
         score=+sucess;
     }
 
-    public boolean Verification(int result, int result2){
+    public void Verification(int result, int result2){
         if(result==result2){
             sucess++;
 
             TextView resultat = (TextView) findViewById(R.id.resultat);
             resultat.setText("Bien joué !!!");
-            return true;
+            triche =1;
+
         }else
         {
             TextView resultat = (TextView) findViewById(R.id.resultat);
             resultat.setText("Dommage !");
-            return false;
+            triche =0;
+
         }
 
     }
