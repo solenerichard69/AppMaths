cb8402814c657d831f279932c771d7c0551ac4b4
Philigaudron <philippine.gaudron@epfedu.fr>
2020-04-06 11:04:20 +0200

    Création nouvelle act

---
+++ app/src/main/java/com/example/premiertest/Sous2.java
@@ -0,0 +1,16 @@
+package com.example.premiertest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sous2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sous2);
+    }
+
+
+}
